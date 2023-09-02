0ee39c04ea5f13e18f62067011747e7c7c32b2e9
moyasuxin <moyasuxin@gmail.com>
2023-09-02 14:33:25 +0800

    graphic

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
-$mysqli = new mysqli("localhost","root","","fyptimetable");
+$mysqli = new mysqli("localhost", "root", "", "fyptimetable");
 // Check connection
-if ($mysqli -> connect_errno) {
-echo "Failed to connect to MySQL: " . $mysqli -> connect_error; exit();
-}else{
-    echo"the database is connected" ;
-    echo "<br>";
+if ($mysqli->connect_errno) {
+    $errorMessage = "Failed to connect to MySQL: " . $mysqli->connect_error;
+    echo "<script>alert('$errorMessage');</script>";
 }
 ?>
