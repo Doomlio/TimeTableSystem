2fb4751a04a87395c29336510d2b975c7cf11969
moyasuxin <moyasuxin@gmail.com>
2023-09-03 18:11:59 +0800

    Update login.php

--- lecturer/login/login.php
+++ lecturer/login/login.php
@@ -5,13 +5,14 @@
     <link rel="stylesheet" href="/asset/adminlogin.css">
     <link rel="stylesheet" href="/asset/valo-button.css">
     <title>Login</title>
-    <a href="/admin/login/adminlogin.php"> admin login</a>
 </head>
 
 <body>
     <div class="container-admin-login">
         <div class="loginbox">
             <h1 class="AGT">Welcome to AGT</h1>
+            <p>If you want to go admin login <a href="/admin/login/adminlogin.php">click here</a></p>
+
             <form method="post" action="loginprocessing.php">
                 <div class="label-container">
                     <input type="email" name="email" placeholder="Email">
