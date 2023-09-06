4dd362ecb785f4a237059e56e1061c20045491a0
Eugene <128957193+Doomlio@users.noreply.github.com>
2023-09-06 13:57:08 +0800

    database

--- database/newestfyptimetable (1).sql
+++ database/newestfyptimetable (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2023 at 10:07 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Sep 06, 2023 at 07:56 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `admin` (
   `adminname` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
@@ -53,7 +53,7 @@ CREATE TABLE `lecturer` (
   `lecemail` varchar(100) NOT NULL,
   `lecpassword` varchar(100) NOT NULL,
   `maxhours` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `lecturer`
@@ -81,20 +81,22 @@ CREATE TABLE `request` (
   `new_venue_id` varchar(11) NOT NULL,
   `status` varchar(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `request`
 --
 
 INSERT INTO `request` (`Reqid`, `lecid`, `timetable_id`, `new_start_time`, `new_end_time`, `new_day`, `new_class_type`, `new_venue_id`, `status`, `created_at`) VALUES
-(34, 2, 19, '14:00:00', '16:00:00', 'monday', 'lab', 'S106', 'approved', '2023-08-27 17:51:29'),
-(35, 2, 32, '12:00:00', '14:00:00', 'tuesday', 'lecture', 'S105', 'deny', '2023-08-27 17:54:35'),
-(36, 2, 19, '14:00:00', '16:00:00', 'monday', 'lab', 'S106', 'approved', '2023-08-29 00:25:11'),
-(37, 2, 33, '08:00:00', '10:00:00', 'friday', 'lecture', 'S105', 'approved', '2023-08-29 05:32:55'),
-(38, 2, 48, '14:00:00', '16:00:00', 'monday', 'lecture', 'S103', 'approved', '2023-08-29 12:00:09'),
-(39, 2, 32, '10:00:00', '12:00:00', 'tuesday', 'lecture', 'S103', 'deny', '2023-08-29 12:00:28'),
-(40, 2, 33, '13:00:00', '15:00:00', 'friday', 'Lab', 'S101', 'approved', '2023-08-31 09:33:48');
+(34, 2, 19, '14:00:00', '16:00:00', 'monday', 'lab', 'S106', 'approved', '2023-08-27 09:51:29'),
+(35, 2, 32, '12:00:00', '14:00:00', 'tuesday', 'lecture', 'S105', 'deny', '2023-08-27 09:54:35'),
+(36, 2, 19, '14:00:00', '16:00:00', 'monday', 'lab', 'S106', 'approved', '2023-08-28 16:25:11'),
+(37, 2, 33, '08:00:00', '10:00:00', 'friday', 'lecture', 'S105', 'approved', '2023-08-28 21:32:55'),
+(38, 2, 48, '14:00:00', '16:00:00', 'monday', 'lecture', 'S103', 'approved', '2023-08-29 04:00:09'),
+(39, 2, 32, '10:00:00', '12:00:00', 'tuesday', 'lecture', 'S103', 'deny', '2023-08-29 04:00:28'),
+(40, 2, 33, '13:00:00', '15:00:00', 'friday', 'Lab', 'S101', 'approved', '2023-08-31 01:33:48'),
+(41, 2, 48, '14:00:00', '16:00:00', 'monday', 'Lab', 'S105', 'pending', '2023-09-04 04:48:15'),
+(42, 2, 32, '15:00:00', '17:00:00', 'wednesday', 'Lab', 'S101', 'pending', '2023-09-04 04:48:31');
 
 -- --------------------------------------------------------
 
@@ -109,22 +111,22 @@ CREATE TABLE `subject` (
   `sem` varchar(100) DEFAULT NULL,
   `lecid` int(11) DEFAULT NULL,
   `course` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `subject`
 --
 
 INSERT INTO `subject` (`subID`, `subname`, `qualification`, `sem`, `lecid`, `course`) VALUES
-('EC1', 'Database Management Fundamentals', 'diploma', 'Oct', 3, 'DIT'),
+('EC1', 'Database Management Fundamentals', 'Diploma', 'Oct', 1, 'DIT'),
 ('EC2', 'Intro to ABC', 'Diploma', 'Oct', 2, 'DIT'),
 ('EC3', 'Intro to DEF', 'Diploma', 'Oct', 2, 'DCS'),
-('EC4', 'Intro to GHI', 'Diploma', 'Oct', 1, 'DCS'),
-('EC5', 'ABCDEFG', 'diploma', 'oct', 1, 'DCS'),
-('EC6', 'Web Development Basics', 'diploma', 'Oct', 3, 'DCS'),
+('EC4', 'Intro to GHI', 'Diploma', 'Oct', 2, 'DCS'),
+('EC5', 'ABCDEFG', 'Diploma', 'oct', 3, 'DCS'),
+('EC6', 'Web Development Basics', 'Diploma', 'Oct', 1, 'DCS'),
 ('EC7', 'Intro to MNO', 'Diploma', 'Oct', 3, 'DCS'),
-('EC8', 'Intro to e', 'Diploma', 'Oct', 2, 'DIT'),
-('EC9', 'Intro to MNOewf', 'Diploma', 'Oct', 1, 'DIT');
+('EC8', 'Intro to e', 'Diploma', 'Oct', 3, 'DIT'),
+('EC9', 'Intro to database', 'diploma', 'Oct', 1, 'May');
 
 -- --------------------------------------------------------
 
@@ -143,25 +145,27 @@ CREATE TABLE `timetable` (
   `venueID` varchar(11) DEFAULT NULL,
   `cstatus` varchar(255) DEFAULT 'active',
   `hours` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `timetable`
 --
 
 INSERT INTO `timetable` (`timetable_id`, `lec_id`, `start_time`, `end_time`, `day`, `classtype`, `subID`, `venueID`, `cstatus`, `hours`) VALUES
-(1, 1, '09:00:00', '11:00:00', 'wednesday', 'lecture', 'EC4', 'S101', 'active', 2),
+(1, 2, '10:00:00', '12:00:00', 'wednesday', 'lecture', 'EC4', 'S101', 'active', 2),
 (2, 3, '11:00:00', '13:00:00', 'monday', 'lecture', 'EC7', 'S101', 'active', 2),
-(14, 1, '08:00:00', '10:00:00', 'wednesday', 'lab', 'EC2', 'S102', 'active', 2),
-(15, 2, '13:00:00', '15:00:00', 'thursday', 'lab', 'EC3', 'S102', 'active', 2),
-(16, 1, '14:00:00', '16:00:00', 'tuesday', 'lab', 'EC9', 'S102', 'active', 2),
-(17, 1, '09:00:00', '11:00:00', 'thursday', 'lecture', 'EC2', 'S101', 'active', 2),
-(19, 1, '15:00:00', '17:00:00', 'friday', 'lab', 'EC9', 'S106', 'active', 2),
-(32, 2, '15:00:00', '17:00:00', 'tuesday', 'lecture', 'EC3', 'S101', 'active', 2),
-(33, 2, '08:00:00', '10:00:00', 'friday', 'lecture', 'EC3', 'S105', 'active', 2),
+(14, 2, '15:00:00', '17:00:00', 'wednesday', 'lab', 'EC2', 'S102', 'active', 2),
+(15, 2, '10:00:00', '12:00:00', 'thursday', 'lab', 'EC3', 'S102', 'active', 2),
+(17, 2, '09:00:00', '11:00:00', 'thursday', 'lecture', 'EC2', 'S101', 'active', 2),
+(32, 2, '13:00:00', '15:00:00', 'tuesday', 'lecture', 'EC3', 'S101', 'active', 2),
+(33, 2, '12:00:00', '14:00:00', 'friday', 'lecture', 'EC3', 'S105', 'active', 2),
 (48, 2, '14:00:00', '16:00:00', 'monday', 'lecture', 'EC3', 'S103', 'active', 2),
-(310, 3, '15:00:00', '17:00:00', 'thursday', 'lab', 'EC1', 'S101011', 'active', 2),
-(311, 3, '15:00:00', '17:00:00', 'friday', 'lecture', 'EC1', 'S101', 'active', 2);
+(310, 1, '09:00:00', '11:00:00', 'thursday', 'lab', 'EC1', 'S101011', 'active', 2),
+(311, 1, '12:00:00', '14:00:00', 'friday', 'lecture', 'EC1', 'S101', 'active', 2),
+(312, 1, '14:00:00', '16:00:00', 'friday', 'lecture', 'EC9', 'S101', 'replacement', 2),
+(316, 1, '13:00:00', '15:00:00', 'monday', 'lecture', 'EC1', 'S101', 'active', 2),
+(317, 1, '13:00:00', '15:00:00', 'thursday', 'lab', 'EC9', 'S104', 'active', 2),
+(318, 1, '15:00:00', '17:00:00', 'tuesday', 'lecture', 'EC9', 'S106', 'active', 2);
 
 -- --------------------------------------------------------
 
@@ -172,7 +176,7 @@ INSERT INTO `timetable` (`timetable_id`, `lec_id`, `start_time`, `end_time`, `da
 CREATE TABLE `venue` (
   `venueid` varchar(11) NOT NULL,
   `venuetype` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `venue`
@@ -250,29 +254,23 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `lecturer`
 --
 ALTER TABLE `lecturer`
-  MODIFY `lec_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `lec_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `request`
 --
 ALTER TABLE `request`
-  MODIFY `Reqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `Reqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `timetable`
 --
 ALTER TABLE `timetable`
-  MODIFY `timetable_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=312;
+  MODIFY `timetable_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=321;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `subject`
---
-ALTER TABLE `subject`
-  ADD CONSTRAINT `fk_subject_lecturer` FOREIGN KEY (`lecid`) REFERENCES `lecturer` (`lec_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `timetable`
